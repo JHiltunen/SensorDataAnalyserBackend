2407d6e2b7e0e92d4864a54173143ebd02f1081d
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-04-21 11:07:40 +0300

    Fix promise not returning

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,31 +1,7 @@
-const path = require('path');
-const fs = require('fs');
+const fileModel = require('../models/fileModel');
 
 const read_file_names = async (req, res) => {
-    //joining path of directory 
-    const directoryPath = path.join("./", 'uploads');
-    //passsing directoryPath and callback function
-    fs.readdir(directoryPath, function (err, files) {
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-
-        files.forEach(function (filename) {
-            fs.readFile(directoryPath + '/' + filename, 'utf8' , (err, data) => {
-                // remove extra ":" from Movesense json 
-                data = data.replace("data:", "data");
-                
-                if (err) {
-                  console.error(err);
-                  return
-                }
-                console.log("DATAAAAAAAAA: ", data);
-                var obj2 = JSON.parse(data);
-                console.log("jkslh", obj2.data.pop());
-            });
-        });
-    });    
+    res.json(await fileModel.readFilesInDirectory());
 };  
 
 module.exports = {
