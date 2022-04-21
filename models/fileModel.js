f672970c586c02c6c6ad993c429af40f330aa9b5
keenanjon <jon.menna@metropolia.fi>
2022-04-21 16:59:53 +0300

    todo list

--- models/fileModel.js
+++ models/fileModel.js
@@ -2,44 +2,52 @@ const path = require('path');
 const fs = require('fs');
 
 const readFilesInDirectory = (file) => {
-    return new Promise((resolve, reject)=>{
-        const filenameArray = [];
-        //joining path of directory 
-        const directoryPath = path.join("./", 'uploads');
-        //passsing directoryPath and callback function
-        fs.readdir(directoryPath, function (err, files) {
-            //handling error
-            if (err) {
-                reject(err);
-            }
-    
-            files.forEach(function (filename) {
-                filenameArray.push(filename);
-    
-                fs.readFile(directoryPath + '/' + filename, 'utf8', (err, data) => {
-                    // remove extra ":" from Movesense json 
-                    data = data.replace("data:", "data");
-    
-                    if (err) {
-                        console.error(err);
-                        return err;
-                    }
+  return new Promise((resolve, reject) => {
+    const filenameArray = [];
+    //joining path of directory
+    const directoryPath = path.join('./', 'uploads');
+    //passsing directoryPath and callback function
+    fs.readdir(directoryPath, function (err, files) {
+      //handling error
+      if (err) {
+        reject(err);
+      }
 
-                    var obj = JSON.parse(data);
-                });
-            });
-            resolve(filenameArray);
-        });    
+      files.forEach(function (filename) {
+        filenameArray.push(filename);
+
+        fs.readFile(directoryPath + '/' + filename, 'utf8', (err, data) => {
+          // remove extra ":" from Movesense json
+          data = data.replace('data:', 'data');
+
+          if (err) {
+            console.error(err);
+            return err;
+          }
+
+          var obj = JSON.parse(data);
+        });
+      });
+      resolve(filenameArray);
     });
+  });
 };
 
+//TODO Ei nyt mulla katos se ajatus päästä!!! --> Vittu voi voi!!!
+
+//TODO funktio joka käy läpi kaikki tiedostot uploads-kansiosta ja palauttaa tiedoston nimet
+
+//TODO funktio joka lukee sen tiedoston, jonka nimi tulee funktioon parametrina
+
+//TODO funktio joka laskee kuukauden keskiarvon (ja jos aikaa on niin kaikki kuukauden datan)
+
+//TODO HUOM! X-CODESSA raja-arvot ja värit (punainen kun poikkeama suuri, ja vihreä kun poikeeama pieni)!!!
+
 const readMonthData = () => {
-    return new Promise((resolve, reject)=>{
-        
-    });
+  return new Promise((resolve, reject) => {});
 };
 
 module.exports = {
-    readFilesInDirectory,
-    readMonthData,
+  readFilesInDirectory,
+  readMonthData,
 };
