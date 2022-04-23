c3541027d345282cc88cf0b24166181e753dc1da
keenanjon <jon.menna@metropolia.fi>
2022-04-23 13:32:42 +0300

    specific file average implemented

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,25 +1,40 @@
 const fileModel = require('../models/fileModel');
 
 const read_file_names = async (req, res) => {
-    res.json(await fileModel.readFilesInDirectory());
-};  
+  res.json(await fileModel.readFilesInDirectory());
+};
 
 const read_month_data = async (req, res) => {
-    res.json(await fileModel.readMonthData());
+  res.json(await fileModel.readMonthData());
 };
 
 const read_file = async (req, res, next) => {
   try {
-   // res.json(await fileModel.readFile("HHUJSFK"));
-    res.json(await fileModel.readFile("20220411T112412Z_174430000206_acc_stream.json"));
+    // res.json(await fileModel.readFile("HHUJSFK"));
+    res.json(
+      await fileModel.readFile('20220411T112412Z_174430000206_acc_stream.json')
+    );
   } catch (error) {
     next(error);
   }
-  
+};
+
+const read_file_and_do_some_math = async (req, res, next) => {
+  try {
+    // res.json(await fileModel.readFile("HHUJSFK"));
+    res.json(
+      await fileModel.readFileAndDoSomeMath(
+        '20220411T112412Z_174430000206_acc_stream.json'
+      )
+    );
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   read_file_names,
   read_month_data,
   read_file,
+  read_file_and_do_some_math,
 };
--- models/fileModel.js
+++ models/fileModel.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-let directoryPath = path.join('./', 'uploads')
+let directoryPath = path.join('./', 'uploads');
 
 const readFilesInDirectory = () => {
   return new Promise((resolve, reject) => {
@@ -19,7 +19,7 @@ const readFilesInDirectory = () => {
       9: [],
       10: [],
       11: [],
-      12: []
+      12: [],
     };
     const months = [];
     //passsing directoryPath and callback function
@@ -31,24 +31,36 @@ const readFilesInDirectory = () => {
 
       files.forEach(function (filename) {
         const dateString = filename.slice(0, 8);
-        console.log("Date: ", dateString);
+        console.log('Date: ', dateString);
         const year = Number(dateString.slice(0, 4));
-        console.log("Year: ", year);
+        console.log('Year: ', year);
         const date = Number(dateString.slice(6, 8));
-        console.log("Date: ", date);
+        console.log('Date: ', date);
         const month = Number(dateString.slice(4, 6));
-        console.log("Month: ", month);
+        console.log('Month: ', month);
 
         const hours = Number(filename.slice(9, 11));
-        console.log("hours: ", hours);
+        console.log('hours: ', hours);
         const minutes = Number(filename.slice(11, 13));
-        console.log("minutes: ", minutes);
+        console.log('minutes: ', minutes);
         const seconds = Number(filename.slice(13, 15));
-        console.log("seconds: ", seconds);
+        console.log('seconds: ', seconds);
 
         // YYYY-MM-DD hh:mm:ss
-        const dateObject = new Date(year + '-' + month + '-' + date + ' ' + hours + ':' + minutes + ':' + seconds);
-        
+        const dateObject = new Date(
+          year +
+            '-' +
+            month +
+            '-' +
+            date +
+            ' ' +
+            hours +
+            ':' +
+            minutes +
+            ':' +
+            seconds
+        );
+
         // use below to get local date and time format
         //console.log("Locale string", dateObject.toLocaleString('fi'));
 
@@ -56,19 +68,19 @@ const readFilesInDirectory = () => {
         if (!months.includes(month)) {
           months.push(month);
         }
-        console.log("Month array: ", months);
-        months.sort(function(a, b) {
+        console.log('Month array: ', months);
+        months.sort(function (a, b) {
           return a - b;
         });
 
-        console.log("Month array after sort: ", months);
+        console.log('Month array after sort: ', months);
 
-        months.forEach(month2 => {
+        months.forEach((month2) => {
           if (month == month2) {
             filesWithMonths[month].push(filename);
           }
         });
-        console.log("Files with months: ", filesWithMonths);
+        console.log('Files with months: ', filesWithMonths);
       });
       resolve(filesWithMonths);
     });
@@ -83,26 +95,82 @@ const readFilesInDirectory = () => {
 // file has to be in /uploads folder
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
-      // check if file exists
-      fs.access(directoryPath + '/' + filename, (err) => {
-        if (err) {
-          console.log("File doesn't exist");
-          reject("File doesn't exist");
-        } else {
-          console.log("File exists")
-          // read file
-          fs.readFile(directoryPath + '/' + filename, 'utf8', (err, data) => {
-            // remove extra ":" from Movesense json
-            data = data.replace('data:', 'data');
-      
-            if (err) {
-              console.error(err);
-              return err;
-            }
-            resolve(JSON.parse(data));
+    // check if file exists
+    fs.access(directoryPath + '/' + filename, (err) => {
+      if (err) {
+        console.log("File doesn't exist");
+        reject("File doesn't exist");
+      } else {
+        console.log('File exists');
+        // read file
+        fs.readFile(directoryPath + '/' + filename, 'utf8', (err, data) => {
+          // remove extra ":" from Movesense json
+          data = data.replace('data:', 'data');
+
+          if (err) {
+            console.error(err);
+            return err;
+          }
+          /*
+          const schlumpf = JSON.parse(data);
+          console.log('SCHLUMPF: ', schlumpf.data.length);
+          var sum = 0.0;
+          schlumpf.data.forEach((floff) => {
+            let x = floff.acc.ArrayAcc[0].x;
+            let y = floff.acc.ArrayAcc[0].y;
+            let z = floff.acc.ArrayAcc[0].z;
+            let sqrtXZ = Math.sqrt(x * x + z * z);
+            let accXYZ = Math.sqrt(y * y + sqrtXZ);
+            sum += accXYZ;
           });
-        } 
-      });
+
+          let average = sum / schlumpf.data.length;
+          console.log(average);
+          */
+          resolve(JSON.parse(data));
+        });
+      }
+    });
+  });
+};
+
+const readFileAndDoSomeMath = (filename) => {
+  return new Promise((resolve, reject) => {
+    // check if file exists
+    fs.access(directoryPath + '/' + filename, (err) => {
+      if (err) {
+        console.log("File doesn't exist");
+        reject("File doesn't exist");
+      } else {
+        console.log('File exists');
+        // read file
+        fs.readFile(directoryPath + '/' + filename, 'utf8', (err, data) => {
+          // remove extra ":" from Movesense json
+          data = data.replace('data:', 'data');
+
+          if (err) {
+            console.error(err);
+            return err;
+          }
+
+          const schlumpf = JSON.parse(data);
+          console.log('SCHLUMPF: ', schlumpf.data.length);
+          var sum = 0.0;
+          schlumpf.data.forEach((floff) => {
+            let x = floff.acc.ArrayAcc[0].x;
+            let y = floff.acc.ArrayAcc[0].y;
+            let z = floff.acc.ArrayAcc[0].z;
+            let sqrtXZ = Math.sqrt(x * x + z * z);
+            let accXYZ = Math.sqrt(y * y + sqrtXZ);
+            sum += accXYZ;
+          });
+
+          let average = sum / schlumpf.data.length;
+          console.log(average);
+          resolve(JSON.parse(data));
+        });
+      }
+    });
   });
 };
 
@@ -114,38 +182,39 @@ const readMonthData = async (month = 0) => {
   // read all files from directory
   // month number is the key and key value is array of file names
   const monthsWithFiles = await readFilesInDirectory();
-  console.log("MonthData ", monthsWithFiles);
+  console.log('MonthData ', monthsWithFiles);
 
   // access specific month with
-  // monthsWithFiles[n] where n is number of the month 
-  console.log("Prints April (Huhtikuu)", monthsWithFiles[4]);
+  // monthsWithFiles[n] where n is number of the month
+  console.log('Prints April (Huhtikuu)', monthsWithFiles[4]);
 
   if (month == 0) {
     // loop trough all months and load corresponding files with readFile(filename) function
-    console.log("Loop all files");
+    console.log('Loop all files');
     for (const monthKey in monthsWithFiles) {
       const monthValue = monthsWithFiles[monthKey];
-      console.log("MonthKey: " + monthKey + " VALUE: " + monthValue);
+      console.log('MonthKey: ' + monthKey + ' VALUE: ' + monthValue);
 
       if (monthValue.length > 0) {
         for (let i = 0; i < monthValue.length; i++) {
           // load each file
           const fileData = await readFile(monthValue[i]);
-          console.log("Month value: ", fileData);
+          console.log('Month value: ', fileData);
+
           // access filedata
-          console.log("DATA: ", fileData.data)
+          console.log('DATA: ', fileData.data);
         }
       }
     }
   } else {
-    // get only specific month data and loadfile with 
-    console.log("Month : " + month + " -> ", monthsWithFiles[month]);
+    // get only specific month data and loadfile with
+    console.log('Month : ' + month + ' -> ', monthsWithFiles[month]);
   }
-  
 };
 
 module.exports = {
   readFilesInDirectory,
   readMonthData,
   readFile,
+  readFileAndDoSomeMath,
 };
