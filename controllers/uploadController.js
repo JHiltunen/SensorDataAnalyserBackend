26ea7869892f5b3c389ce7b2c77c9dd3ec6412df
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-04-29 09:27:59 +0300

    Allow user to upload multiple files

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -2,7 +2,7 @@
 const uploadModel = require('../models/uploadModel');
 
 const upload_json_file = async (req, res, next) => {
-  const file = req.file;
+  const file = req.files;
   if (!file) {
     const error = new Error('Please upload a json file');
     error.httpStatusCode = 415;
